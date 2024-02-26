ce66939499beeab58bc5971128ea931df5b7a6c5
DevSecCon <149763747+DevSecCon@users.noreply.github.com>
2024-02-26 14:22:39 +0600

    Update entrypoint.sh
    Signed-off-by: DevSecCon <149763747+DevSecCon@users.noreply.github.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-# configure Vul to use the khulnasoft plugin
+# configure Trivy to use the aqua plugin
 
-if [ -n "${INPUT_KHULNASOFT_CSPM_URL}" ]; then
-  export CSPM_URL=${INPUT_KHULNASOFT_CSPM_URL}
+if [ -n "${INPUT_AQUA_CSPM_URL}" ]; then
+  export CSPM_URL=${INPUT_AQUA_CSPM_URL}
 fi
 
-if [ -n "${INPUT_KHULNASOFT_API_URL}" ]; then
-  export KHULNASOFT_URL=${INPUT_KHULNASOFT_API_URL}
+if [ -n "${INPUT_AQUA_API_URL}" ]; then
+  export AQUA_URL=${INPUT_AQUA_API_URL}
 fi
 
 # if GITHUB_WORKSPACE is defined, move to it
@@ -20,13 +20,13 @@ fi
 # check if any custom build name has been specified
 BUILD=${INPUT_BUILD:-GitHubAction}
 
-# Execute Vul
-export VUL_RUN_AS_PLUGIN=khulnasoft
-vul iac --tags "system:github" \
+# Execute Trivy
+export TRIVY_RUN_AS_PLUGIN=aqua
+trivy iac --tags "system:github" \
           --tags "build:${BUILD}" \
           --tags "repository:${GITHUB_REPOSITORY}" \
           --tags "branch:${GITHUB_REF_NAME}" \
           --tags "git_user:${GITHUB_ACTOR}" \
-          --khulnasoft-key "${INPUT_KHULNASOFT_KEY}" \
-          --khulnasoft-secret "${INPUT_KHULNASOFT_SECRET}" \
+          --aqua-key "${INPUT_AQUA_KEY}" \
+          --aqua-secret "${INPUT_AQUA_SECRET}" \
           "${GITHUB_WORKSPACE}"
